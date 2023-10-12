b43e82e8dae3b278d0b5b7aeeaaf2fb5f68f930e
su0814 <107176171+su0814@users.noreply.github.com>
2023-10-12 09:37:09 +0800

    增加启动图

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QDesktopWidget>
+#include <QSplashScreen>
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    MainWindow   w;
+
+    QDesktopWidget desktop;
+    QRect          screenGeometry = desktop.screenGeometry(QCursor::pos());
+    QPixmap        splashImage(":/new/photo/photo/SPC100.png");
+    QSplashScreen  splash(splashImage);
+    splash.move(screenGeometry.center() - splash.rect().center());  // 将启动图片移动到屏幕中央
+    splash.show();
+    MainWindow w;
+    splash.finish(&w);
     w.show();
 
     return a.exec();
